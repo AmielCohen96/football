cf7d053bf4dbd59fd39ccb03291f4f42997a8965
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2022-12-28 10:29:17 +0200

    Ori not gay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+      team_name: 'none',
+      result: 0,
+      teamsData: [],
+      teamsName: '',
+      leagueData: [],
+      leagueName: 'none',
+      leagueId: ''
+  }
+  leagues_temp = []
+  teams_temp = []
+
+  leagueNameChanged = (event) => {
+    this.setState({
+      leagueName: event.target.value
+    })
+  }
+
+  teamNameChanged = (event) => {
+        this.setState({
+            leagueName: event.target.value
+        })
+    }
+
+  getLeagueData = () => {
+      axios.get("https://app.seker.live/fm1/leagues")
+        .then((response) => {
+            response.data.map((item) => {
+                this.leagues_temp.push(item.name)
+            })
+            this.setState({
+                leagueData: this.leagues_temp
+              })
+        });
+  }
+
+  getTeamData = () => {
+      axios.get("https://app.seker.live/fm1/teams/"+ this.state.leagueId)
+          .then((response) => {
+              response.data.map((item) => {
+                  this.teams_temp.push(item.name)
+              })
+              this.setState({
+                  teamsData: this.teams_temp
+              })
+          });
+    }
+
+  componentDidMount() {
+    this.getLeagueData();
+    this.getTeamData();
+  }
+
+
+  render() {
+    return (
+        <div className="App">
+            <div style={{fontWeight: "bold", fontSize: "20px"}}>leagues:</div>
+            <select value={this.state.leagueName} onChange={this.leagueNameChanged}>
+              <option value={"none"} disabled={true}>SELECT LEAGUE</option>
+              {
+                this.state.leagueData.map((item) => {
+                  return (
+                      <option value={item}>{item}</option>
+                  )
+                })
+              }
+            </select>
+            <select value={this.state.team_name} onChange={this.teamNameChanged}>
+                <option value={"none"} disabled={true}>SELECT TEAM</option>
+                {
+                    this.state.teamsData.map((item) => {
+                        return (
+                            <option value={item}>{item}</option>
+                        )
+                    })
+                }
+            </select>
+        </div>
+      );
+    }
 }
 
 export default App;
+
